cf7b45f044580d9952a1e80696590bf391df8b42
kt <khataylor@gmail.com>
2026-03-18 17:55:00 -0400

    Update ec2_metadata.sh

--- scripts/aws/ec2_metadata.sh
+++ scripts/aws/ec2_metadata.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
-TOKEN=$(curl -s -X PUT "http://<host/ip>/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document
+TOKEN=$(curl -s -X PUT "http://<hostname/ip>/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://<hostname/ip>/latest/dynamic/instance-identity/document
